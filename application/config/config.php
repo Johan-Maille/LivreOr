a436b575d976c4d413af4290033bb90039fed7eb
Johan-Maille <johan73460@outlook.fr>
2018-09-06 15:23:33 +0200

    Changement de la base_url plus suppression du config index_page

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/codeigniter/';
-$config['base_url'] = 'http://localhost/Codeigniter/';
+$config['base_url'] = 'http://localhost/codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +36,8 @@ $config['base_url'] = 'http://localhost/Codeigniter/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
