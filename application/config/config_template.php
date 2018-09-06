96cec346f8a54692e5115ea1f673a95cebff39b1
Johan-Maille <johan73460@outlook.fr>
2018-09-06 16:15:04 +0200

    Mise a jour du ficher + suppression des information confidentiel.

--- application/config/config_template.php
+++ application/config/config_template.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/codeigniter/';
-$config['base_url'] = 'http://localhost/codeigniter/index.php';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,7 +35,6 @@ $config['base_url'] = 'http://localhost/codeigniter/index.php';
 | variable so that it is blank.
 |
 */
-// $config['index_page'] = 'index.php';
 $config['index_page'] = '';
 
 /*
